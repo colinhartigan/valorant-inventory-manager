4bc95320a674969c1541954b44937b1326af18ad
Colin <colinjoe9@gmail.com>
2021-12-01 22:44:34 -0500

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,4 +1,12 @@
+import os, traceback
 from src.server import Server
 
 if __name__ == "__main__":
-    Server.start()
+    try:
+        Server.start()
+    except:
+        print("error: please create an issue with the traceback below if this problem persists")
+        traceback.print_exc()
+        input("press enter to exit...")
+        os._exit(1)
+    
