c8717e8111deda9b6a077973c292ab320acb61a1
PH-XRGN <105039359+PH-XRGN@users.noreply.github.com>
2022-05-06 22:11:02 +0800

    Update client_config.py

--- server/src/client_config.py
+++ server/src/client_config.py
@@ -9,7 +9,7 @@ IS_TEST_BUILD = False # directs to the test client appdata directory
 DEBUG = False # prints all log messages to console
 
 FORCE_DEFAULT_SKINS = False # only deafult skins unlocked
-UNLOCK_ALL_SKINS = False # just for testing purposes, doesn't actually unlock anything
+UNLOCK_ALL_SKINS = True # just for testing purposes, doesn't actually unlock anything
 UNLOCK_ALL_BUDDIES = False # just for testing purposes, doesn't actually unlock anything
 
 USE_TEST_DIRECTORY = False # use a different directory for testing purposes
